a3d87d77307c3a5052c4a2637ffc252532d98d86
Michael Fluharty <michael.fluharty@mac.com>
2025-10-25 08:40:51 -0500

    Initial Commit

---
+++ GIT IssueTracker Light/GIT_IssueTracker_LightApp.swift
@@ -0,0 +1,17 @@
+//
+//  GIT_IssueTracker_LightApp.swift
+//  GIT IssueTracker Light
+//
+//  Created by Michael Fluharty on 10/25/25.
+//
+
+import SwiftUI
+
+@main
+struct GIT_IssueTracker_LightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GIT IssueTracker LightTests/GIT_IssueTracker_LightTests.swift
@@ -0,0 +1,16 @@
+//
+//  GIT_IssueTracker_LightTests.swift
+//  GIT IssueTracker LightTests
+//
+//  Created by Michael Fluharty on 10/25/25.
+//
+
+import Testing
+
+struct GIT_IssueTracker_LightTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
